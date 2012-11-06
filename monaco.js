f5c4402bd08cc613eec850f6f3d38f2dc6987726
marcos <marcos@wattpad.com>
2012-11-06 10:42:06 -0500

    added fix for certain android devices when executing JSON.parse(null) statements

--- monaco.js
+++ monaco.js
@@ -638,4 +638,14 @@
     });
 
     Application.extend = Form.extend = Transition.extend = extend;
+
+    // Certain Android devices are having issues when a JSON.parse(null) call is executed.
+    // the following should fix this bug 
+    JSON.originalParse = JSON.parse;
+    JSON.parse = function(text) {
+        if (text) {
+            return JSON.originalParse(text);
+        }
+        return null;
+    };
 }(window));
