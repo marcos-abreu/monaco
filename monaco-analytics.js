6974434cbc0f045eba16e7dcc7701f06969e9ea9
Marcos <abreu.marcos@gmail.com>
2013-08-25 16:46:01 -0400

    added first implementation of monaco-analytics

---
+++ monaco-analytics.js
@@ -0,0 +1,24 @@
+// override Backbone loadURL to track page loads on history.navigate calls
+(function(window) {
+
+    var loadUrl = window.Backbone.History.prototype.loadUrl;
+
+    // overriden method to inject a call to track page views
+    Backbone.History.prototype.loadUrl = function() {
+        var matched = loadUrl.apply(this, arguments),
+            pvFragment = this.fragment;
+        if (!/^\//.test(pvFragment)) {
+            pvFragment = '/' + pvFragment;
+        }
+        this.trackPageview(pvFragment);
+        return matched;
+    };
+
+    // Override this method if you are not using google analytics, but 
+    // instead another analytics service to track page views
+    Backbone.History.prototype.trackPageview = function(fragment) {
+        if( window.ga !== void 0 ) {
+            window.ga('send', 'pageview', fragment);
+        }
+    }
+}(window));
