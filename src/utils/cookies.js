8d1cc73ff678127c99d78370b9235b583600ed99
marcos <marcos@wattpad.com>
2013-09-04 14:43:15 -0400

    fixed day parsing issue from monaco cookie utils

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,4 +1,4 @@
-(function(window){
+(function(window, _){
     'use strict';
 
     var Monaco = window.Monaco = (window.Monaco || {});
@@ -7,7 +7,7 @@
     Monaco.utils = Monaco.utils || {};
 
     Monaco.utils.setCookie = function( key, value, days, baseDomain ) {
-        days = ( days && days >= 0 ) ? days : 365;
+        days = ( days && _.isNumber(days) ) ? days : 365;
 
         var date = new Date();
         date.setTime( date.getTime() + ( days * 24 * 60 * 60 * 1000 ) );
@@ -28,4 +28,4 @@
         return ( result = new RegExp( '(?:^|; )' + encodeURIComponent( key ) + '=([^;]*)' ).exec( document.cookie ) ) ? result[1] : null;
     };
 
-}(window));
+}(window, window._));
