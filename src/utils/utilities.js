46c3ca4ea5a974c4b9bb28df8193a591b3a270cd
marcos <marcos@wattpad.com>
2013-09-04 14:44:56 -0400

    added the android parsing fix to the utilities file

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,13 +1,14 @@
-(function(window) {
+(function(window, _) {
     'use strict';
 
     // Certain Android devices are having issues when a JSON.parse(null) call is executed.
     // the following should fix this bug
-    JSON.originalParse = JSON.parse;
+
+    var parse = JSON.parse;
     JSON.parse = function(text) {
         if (text) {
-            return JSON.originalParse(text);
+            return parse(text);
         }
         return null;
     };
-}(window));
+}(window, window._));
